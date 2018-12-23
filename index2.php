88033bca26507f603d12ff6cfede728d0a0c8284
vaco <armando.catota@hotmail.com>
2018-12-23 14:59:02 -0500

    Agrgado login, css

---
+++ index2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Aprende|GIT</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="assets/css/estilo1.css">
+</head>
+<body>
+	<br>
+	<h1 class="text-center">Git && gitHub</h1>
+	<hr>
+
+	<div class="container">
+<div class="card card-body bg-light">
+
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+</div>
+	</div>
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Aprende|GIT</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="assets/css/estilo1.css">
+	<style type="text/css">
+		*{
+			/*background-color:  #9fbcf9;*/
+		}
+	</style>
+</head>
+<body>
+	<br>
+	<h1 class="text-center">Login a Git</h1>
+	<hr>
+
+	<div class="container">
+		<div class="row  justify-content-center"><!-- Centrar un DIV -->
+		<!-- <div class="row  justify-content-center align-items-center "> -->
+			<div class="card card-body bg-light col-md-5">
+					<form>
+					  <div class="form-group">
+					    <label for="exampleInputEmail1">Dirección Correo</label>
+					    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingrese correo">
+					    <small id="emailHelp" class="form-text text-muted">Nosotros nunca compartiremos tu correo con nadie</small>
+					  </div>
+					  <div class="form-group">
+					    <label for="exampleInputPassword1">Clave</label>
+					    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Clave">
+					  </div>
+					 <!--  <div class="form-group form-check">
+					    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+					    <label class="form-check-label" for="exampleCheck1">Recordarme</label>
+					  </div> -->
+					  <div class="form-group text-right">
+					  <button type="submit" class="btn btn-warning">Ingresar</button>
+					</div>
+					</form>
+			</div>
+		</div>
+</div>
+</body>
+</html>
